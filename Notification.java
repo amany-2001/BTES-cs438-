629f154d3148e7631831fb438f4dd9ef9e2d824c
NoorAlDonia22 <no.elmestiri@uot.edu.ly>
2024-10-28 23:56:55 +0200

    Add files via upload

---
+++ Notification.java
@@ -0,0 +1,12 @@
+public class Notification {
+    private int notiID;
+    private int userID;
+    private int eventID;
+    private String notiType;
+    private String message;
+
+    public boolean sent_message(int userID,String notiType,String message){
+        return false;
+        
+    }
+}
