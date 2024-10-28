4210c7f120173500ca842f617b7dccbf26462c8d
NoorAlDonia22 <no.elmestiri@uot.edu.ly>
2024-10-28 23:59:22 +0200

    Add files via upload

---
+++ User.java
@@ -0,0 +1,35 @@
+public class User {
+    private int userID;
+    private String userName;
+    private int userAge;
+    private String email;
+    private int point;
+    private String accountNumber;
+    private String password;
+    private String phone;
+
+    public Event[] search(){
+        return null;
+
+    }
+    public Ticket bookTicket(String seatNumber,int eventID){
+        return null;
+        
+    }
+    public String rateEvent(int eventID){
+        return "";
+        
+    }
+    public boolean sellTicket(String ticketID){
+        return false;
+
+    }
+    public int getAge(){
+        return userAge;
+        
+    }
+    public int getPoint(){
+        return point;
+        
+    }
+}
