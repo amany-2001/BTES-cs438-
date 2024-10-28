fcbcd81022870ffdfd91cccc7a398b82561a4f64
NoorAlDonia22 <no.elmestiri@uot.edu.ly>
2024-10-28 23:57:49 +0200

    Add files via upload

---
+++ Ticket.java
@@ -0,0 +1,21 @@
+public class Ticket {
+
+    private String ticketID;
+    private String eventName;
+    private String seatNumber;
+    private float price;
+    private String status;
+
+    public boolean refund(){
+        return false;
+
+    }
+    public boolean sell(){
+        return false;
+
+    }
+    public boolean purchase(){
+        return false;
+
+    }
+}
