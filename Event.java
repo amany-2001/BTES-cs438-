ae40584a7a33091eece604280a72c0f74a384db2
NoorAlDonia22 <no.elmestiri@uot.edu.ly>
2024-10-28 23:51:23 +0200

    Add files via upload

---
+++ Event.java
@@ -0,0 +1,23 @@
+import java.sql.Date;
+
+public class Event {
+    private int eventID;
+    private String eventName;
+    private String catogary;
+    private Date date;
+    private String location;
+    private Seat avaliablleSeats;
+
+    public Event display_ditals(){
+        return null;
+        
+    }
+    public Seat[] get_avalible_seats(){
+        return null;
+        
+    }
+    public boolean researv_seat(String seatNumber){
+        return false;
+
+    }
+}
