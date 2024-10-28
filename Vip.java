3a57c53eb132a178d3dc3e4196b3d55717ac8080
NoorAlDonia22 <no.elmestiri@uot.edu.ly>
2024-10-29 00:03:35 +0200

    Add files via upload

---
+++ Vip.java
@@ -0,0 +1,15 @@
+public class Vip {
+    private int userID;
+    private int eventID;
+    private Seat vipSeat;
+    private String addtionServes;
+
+    public Seat getVipSeat(){
+        return vipSeat;
+        
+    }
+    public String[] offerVIPserves(){
+        return null;
+        
+    }
+}
