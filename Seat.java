fdccf65b74364c07a6311e96c068993af1a9e207
NoorAlDonia22 <no.elmestiri@uot.edu.ly>
2024-10-29 19:42:59 +0200

    Add files via upload

--- Seat.java
+++ Seat.java
@@ -1,14 +1,27 @@
 
 public class Seat {
-    private String seatNumber;
+    private int seatNumber;
     private boolean isAvailable;
-
-    public Seat[] choose_seat(){
-        return null;
-        
+    public Seat(int seatNumber, boolean isAvailable) {
+        this.seatNumber = seatNumber;
+        this.isAvailable = isAvailable;
     }
-    public boolean reserv_seats(){
-        return isAvailable;
+    public int getSeatNumber() {
+        return seatNumber;
+    }
+    void setAvailable(boolean isAvailable) {
+        this.isAvailable=isAvailable;
+    }
+    public boolean choose_seat(){
+        if (isAvailable) {
+            setAvailable(false);
+            return true; // تم اختيار المقعد بنجاح
+        } else {
+            return false; // المقعد غير متاح
+        }
+    }
+    public void reserv_seats(boolean available){
+        isAvailable = available;
         
     }
     public boolean is_seat_available(){
---
+++ db.java
@@ -0,0 +1,50 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class db {
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/btes";
+        String usr = "root";
+        String password = " ";
+
+        List<User> users = new ArrayList<>();
+        List<Ticket> tickets = new ArrayList<>();
+        List<Event> events = new ArrayList<>();
+        List<Notification> notifications = new ArrayList<>();
+        List<Payment> payments = new ArrayList<>();
+        List<Discount> discounts = new ArrayList<>();
+        List<Vip> vips = new ArrayList<>();
+        List<Seat> seats = new ArrayList<>();
+        
+
+        try {
+            Connection connection = DriverManager.getConnection(url, usr, password);
+            Statement statement = connection.createStatement();
+
+            // استرجاع بيانات المستخدم
+            ResultSet resultusers = statement.executeQuery("SELECT * FROM user");
+            while (resultusers.next()) {
+                User user = new User(resultusers.getInt("id"), resultusers.getString("username"), resultusers.getInt("userAge"),resultusers.getString("phone"),resultusers.getString("email"),resultusers.getString("accountNumber"),resultusers.getString("password"),resultusers.getInt("point"));
+                users.add(user);
+            }
+
+            // استرجاع بيانات الحدت
+            
+
+            resultusers.close();
+            
+
+            statement.close();
+            connection.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
+
