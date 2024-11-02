7c3e6812e85ff74c655e263a9d336e42f79259b2
amanybellazi <157745200+amany-2001@users.noreply.github.com>
2024-11-02 20:07:13 +0200

    Delete Discount.java

--- Discount.java
+++
@@ -1,9 +0,0 @@
-public class Discount {
-    private int discountID;
-    private float discountAmount;
-
-    public float apply_Discount(int point,int age){
-        return age;
-        
-    }
-}
