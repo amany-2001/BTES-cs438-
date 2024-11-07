2f17b389a4f0043dceed818ab2ce6d933de57e27
amanybellazi <157745200+amany-2001@users.noreply.github.com>
2024-11-07 12:45:48 +0200

    Update booking.php

--- booking.php
+++ booking.php
@@ -1,20 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title>biik ticket</title>
-    </head>
-    <body>
-    <h2>حجز تذكرة</h2>
+<head>
+    <meta charset="UTF-8">
+    <title>Book Ticket</title>
+    <link rel="stylesheet" href="style.css"> 
+</head>
+<body>
+    <div class="page-header">
+        <h1>Book a Ticket</h1>
+    </div>
     <form action="action.php" method="post">
         <input type="hidden" name="action" value="bookTicket">
+        <?php
+        if (isset($_POST['seat_ids']) && isset($_POST['event_id'])) {
+            foreach ($_POST['seat_ids'] as $seat_id) {
+                echo "<input type='hidden' name='seat_ids[]' value='$seat_id'>";
+            }
+            echo "<input type='hidden' name='event_id' value='" . $_POST['event_id'] . "'>";
+        }
+        ?>
         <label for="user_id">معرف المستخدم:</label>
-        <input type="number" name="user_id" required><br>
-        <label for="seatnumber">رقم المقعد:</label>
-        <input type="text" name="seatnumber" required><br>
-        <label for="eventname">اسم الحدث:</label>
-        <input type="text" name="eventname" required><br>
-        <button type="submit">حجز التذكرة</button>
+        <input type="text" name="user_id" required><br>
+
+        <label for="password">كلمة المرور:</label>
+        <input type="password" name="password" required><br>
+
+        <button type="submit">تأكيد الحجز</button>
     </form>
-    </body>
+</body>
 </html>
