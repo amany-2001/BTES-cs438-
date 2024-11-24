9cc98bdfe2723d3506b72f6c093aaf9823f7555f
NoorAlDonia22 <no.elmestiri@uot.edu.ly>
2024-11-24 22:45:13 +0200

    Update MobiCashPayment.php

--- MobiCashPayment.php
+++ MobiCashPayment.php
@@ -3,16 +3,21 @@ class MobicashPayment extends Payment {
     private $email;
     private $phone;
 
-    public function __construct($db, $userID, $amount, $ticketID, $email, $phone) {
-        parent::__construct($db, $userID);  // استدعاء الكونستركت من الكلاس الأب
-        $this->amount = $amount;  
-        $this->ticketID = $ticketID;  
-        $this->email = $email;
-        $this->phone = $phone;
-    }
+    // public function __construct($db, $userID, $amount, $ticketID, $email, $phone) {
+    //     parent::__construct($db);  // استدعاء الكونستركت من الكلاس الأب
+    //     $this->amount = $amount;  
+    //     $this->ticketID = $ticketID;  
+    //     $this->email = $email;
+    //     $this->phone = $phone;
+    // }
 
     // تنفيذ الدفع عبر خدمة موباي كاش
     public function processPayment(): bool {
+       if (isset($data['phone_number'], $data['email'])) {
+            $this->phoneNumber = $data['phone_number'];
+            $this->email = $data['email'];
+            return is_numeric($this->phoneNumber) && filter_var($this->email, FILTER_VALIDATE_EMAIL);
+        }
         return false;
     }
 }
