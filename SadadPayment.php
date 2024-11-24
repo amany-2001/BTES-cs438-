92e7fa4bc6b5c56993f8379b146a2cd50e5f0b76
NoorAlDonia22 <no.elmestiri@uot.edu.ly>
2024-11-24 22:46:49 +0200

    Update SadadPayment.php

--- SadadPayment.php
+++ SadadPayment.php
@@ -1,13 +1,13 @@
 <?php
 class SdadPayment extends Payment {
-    private $storeNumber;
+    public $code;
 
-    public function __construct($db, $userID, $amount, $ticketID, $storeNumber) {
-        parent::__construct($db, $userID);  // استدعاء الكونستركت من الكلاس الأب
-        $this->amount = $amount;  
-        $this->ticketID = $ticketID;  
-        $this->storeNumber = $storeNumber;
-    }
+    // public function __construct($db, $userID, $amount, $ticketID, $storeNumber) {
+    //     parent::__construct($db, $userID);  // استدعاء الكونستركت من الكلاس الأب
+    //     $this->amount = $amount;  
+    //     $this->ticketID = $ticketID;  
+    //     $this->storeNumber = $storeNumber;
+    // }
 
     // تنفيذ الدفع عبر خدمة سداد
     public function processPayment(): bool {
