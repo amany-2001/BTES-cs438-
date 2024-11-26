82d15ccccfe7f9831e808797e717e20a1ff03daf
NoorAlDonia22 <no.elmestiri@uot.edu.ly>
2024-11-26 16:56:05 +0200

    Update CardPayment.php

--- CardPayment.php
+++ CardPayment.php
@@ -1,20 +1,12 @@
+
 <?php
 
 class CardPayment extends Payment {
-    private $accountNumber;
-    private $password;
+    public $accountNumber;
+    public $code;
 
-    // public function __construct($db, $userID, $ticketID, $amount, $accountNumber, $password) {
-    //     parent::__construct($db, $userID);  // استدعاء الكونستركت من الكلاس الأب
-    //     $this->ticketID = $ticketID;       
-    //     $this->amount = $amount;           
-    //     $this->accountNumber = $accountNumber;
-    //     $this->password = $password;
-    }
-
-    // تنفيذ عملية الدفع عبر البطاقة المصرفية
-    public function processPayment(): bool {
-       if (isset($data['account_number'], $data['code'])) {
+    public function processPayment($data) {
+        if (isset($data['account_number'], $data['code'])) {
             $this->accountNumber = $data['account_number'];
             $this->code = $data['code'];
             return is_numeric($this->accountNumber) && is_numeric($this->code);
