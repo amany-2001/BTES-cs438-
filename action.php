ffdb3389de33bec6aa471eb293a4006bfd9114e5
amanybellazi <157745200+amany-2001@users.noreply.github.com>
2024-12-11 12:40:11 +0200

    Update action.php

--- action.php
+++ action.php
@@ -3,10 +3,8 @@ include_once 'database.php';
 include_once 'User.php';
 include_once 'Event.php';
 include_once 'Ticket.php';
-include_once 'Payment.php';
-include_once 'CardPayment.php';
-include_once 'MobiCashPayment.php';
-include_once 'SadadPayment.php';
+include_once 'Seat.php'; 
+
 
 // إنشاء اتصال بقاعدة البيانات
 $database = new Database();
@@ -15,11 +13,13 @@ $db = $database->getConnection();
 $action = $_POST['action'];
 
 
+
+
 if ($action == 'details') {
-    $event_id = isset($_POST['event_id']) ? $_POST['event_id'] : '';
-    if ($event_id) {
+    $eventid = isset($_POST['eventid']) ? $_POST['eventid'] : '';
+    if ($eventid) {
         $event = new Event($db);
-        $stmt = $event->displayDetails($event_id);
+        $stmt = $event->displayDetails($eventid);
         echo "<h2>تفاصيل الحدث</h2>";
         while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
             echo "event name " . $row['eventname'] . "<br>";
@@ -30,7 +30,7 @@ if ($action == 'details') {
 
         // عرض المقاعد المتاحة
         $seat = new Seat($db);
-        $availableSeats = $seat->getSeatsByEvent($event_id);
+        $availableSeats = $seat->getSeatsByEvent($eventid);
 
         echo "<form action='booking.php' method='post'>";
         echo "<table border='1'>";
@@ -38,40 +38,45 @@ if ($action == 'details') {
         foreach ($availableSeats as $seat) {
             echo "<tr>";
             echo "<td>" . $seat['seatnumber'] . "</td>";
-            echo "<td>". "</td>"; ////////المفروض ان نضيفو السعر هنا 
-            echo "<td><input type='checkbox' name='seat_ids[]' value='" . $seat['seatid'] . "'></td>";
+            echo "<td>" . $seat['price'] . "</td>"; ////////المفروض ان نضيفو السعر هنا 
+            echo "<td><input type='checkbox' name='seat_id[]' value='" . $seat['seat_id'] . "'></td>";
             echo "</tr>";
         }
         echo "</table>";
-        echo "<input type='hidden' name='event_id' value='" . $event_id . "'>";
+        echo "<input type='hidden' name='eventid' value='" . $eventid . "'>";
         echo "<button type='submit'> go to booking </button>";
         echo "</form>";
     } else {
         echo "error:event id not passed";
     }
-} elseif ($action == 'bookTicket') {
-    $user_id = $_POST['user_id'];
-    $password = $_POST['password'];
-    $event_id = $_POST['event_id'];
-    $seat_ids = $_POST['seat_ids'];
-    $paymentType = $_POST['payment_method'];
-    $paymentData = $_POST['payment_data'];
-
-    $ticket = new Ticket($db);
-    $user = new User($db);
-    $event = new Event($db);
-
-    $stmt = $event->displayDetails($eventid);
-    $eventDetails = $stmt->fetch(PDO::FETCH_ASSOC); // الحصول على تفاصيل الحدث
-    try {
-        $ticket->bookWithPayment($userID, $seat_id, $eventid, $paymentType, $paymentData);
-        echo "Ticket booked successfully with payment.";
-    } catch (Exception $e) {
-        echo "Error: " . $e->getMessage();
-    }
 
    
-} elseif ($action == 'refundTicket') {
+
+} elseif ($action == 'bookTicket') {
+        $user_id = $_POST['user_id'];
+        $eventid = $_POST['eventid'];
+        $seat_id = $_POST['seat_id'];
+       
+        $ticket = new Ticket($db);
+        $user = new User($db);
+        $event = new Event($db);
+        try {
+            $ticket->bookTicket($user_id, $seat_id, $eventid);
+
+            echo "Ticket booked successfully .";
+        } catch (Exception $e) {
+            echo "Error: " . $e->getMessage();
+        }
+        
+    
+
+    }
+    //$stmt = $event->displayDetails($eventid);
+    //$eventDetails = $stmt->fetch(PDO::FETCH_ASSOC); // الحصول على تفاصيل الحدث
+   
+
+   
+    if ($action == 'refundTicket') {
     $ticket_id = $_POST['ticket_id'];
 
     $user = new User($db);
